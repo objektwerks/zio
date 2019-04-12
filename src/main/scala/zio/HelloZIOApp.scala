ace528e8aaf161e19080f1022fae10b09912e1f5
objektwerks <objektwerks@runbox.com>
2019-04-12 14:36:22 -0400

    added error and success constants

--- src/main/scala/zio/HelloZIOApp.scala
+++ src/main/scala/zio/HelloZIOApp.scala
@@ -4,7 +4,10 @@ import scalaz.zio.console.{Console, getStrLn, putStrLn}
 import scalaz.zio.{App, ZIO}
 
 object HelloZIOApp extends App {
-  def run(args: List[String]): ZIO[Console, Nothing, Int] = program.fold(_ => 1, _ => 0)
+  val error = 1
+  val success = 0
+
+  def run(args: List[String]): ZIO[Console, Nothing, Int] = program.fold(_ => error, _ => success)
 
   val program = for {
     _ <- putStrLn("Hello! What is your name?")
