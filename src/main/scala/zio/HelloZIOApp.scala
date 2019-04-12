3d5b29f54618f70645dd78a67a2b1b78ff53778f
objektwerks <objektwerks@runbox.com>
2019-04-12 14:29:13 -0400

    added introductory hello zio console app

---
+++ src/main/scala/zio/HelloZIOApp.scala
@@ -0,0 +1,14 @@
+package zio
+
+import scalaz.zio.console._
+import scalaz.zio.{App, ZIO}
+
+object HelloZIOApp extends App {
+  def run(args: List[String]): ZIO[Console, Nothing, Int] = program.fold(_ => 1, _ => 0)
+
+  val program = for {
+    _ <- putStrLn("Hello! What is your name?")
+    n <- getStrLn
+    _ <- putStrLn(s"Hello, $n, welcome to ZIO!")
+  } yield ()
+}
