87b38199d7fd1d7251c0b3c812ecc0ff19db9164
objektwerks <objektwerks@runbox.com>
2019-04-26 14:08:47 -0400

    reformatted zio test; added empty fiber test

--- src/test/scala/zio/ZioTest.scala
+++ src/test/scala/zio/ZioTest.scala
@@ -13,26 +13,33 @@ class ZioTest extends FunSuite with Matchers {
 
   test("effects") {
     runtime.unsafeRun( ZIO.fail("fail").mapError(error => new Exception(error)).either ).left.toOption.nonEmpty shouldBe true
+
     runtime.unsafeRun( ZIO.succeed(33) ) shouldBe 33
     runtime.unsafeRun( ZIO.succeedLazy( List(1, 2, 3).sum ) ) shouldBe 6
+
     runtime.unsafeRun( ZIO.fromOption(Some(3)).map(_ * 2) ) shouldBe 6
     runtime.unsafeRun( ZIO.fromEither(Right(3)).map(_ * 2) ) shouldBe 6
     runtime.unsafeRun( ZIO.fromTry(Try(18 / 3)) ) shouldBe 6
     runtime.unsafeRun( ZIO.fromFunction((i: Int) => i * i).provide(3) ) shouldBe 9
     runtime.unsafeRun( ZIO.fromFuture { implicit ec => Future(3).map(_ * 3) } ) shouldBe 9
-    runtime.unsafeRun( ZIO.effect( Source.fromFile("build.sbt", utf8) ) ).mkString.nonEmpty shouldBe true
   }
 
   test("errors") {
     val invalidFileEffect = ZIO.effect( Source.fromFile("build.sat", utf8) )
     val validFileEffect = ZIO.effect( Source.fromFile("build.sbt", utf8) )
+
     val fallback = invalidFileEffect orElse validFileEffect
     val fold: Task[BufferedSource] = invalidFileEffect.foldM(_ => validFileEffect, source => ZIO.succeed(source))
     val catchall: Task[BufferedSource] = invalidFileEffect.catchAll(_ => validFileEffect)
     val retryOrElse = invalidFileEffect.retryOrElse( Schedule.once, (_: Throwable,_: Unit) => validFileEffect)
+
     runtime.unsafeRun( fallback ).mkString.nonEmpty shouldBe true
     runtime.unsafeRun( fold ).mkString.nonEmpty shouldBe true
     runtime.unsafeRun( catchall ).mkString.nonEmpty shouldBe true
     runtime.unsafeRun( retryOrElse ).mkString.nonEmpty shouldBe true
+  }
+
+  test("fibers") {
+
   }
 }
