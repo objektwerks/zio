129e9f6513c396650e913a11f1be5983f180f6b4
objektwerks <objektwerks@runbox.com>
2019-04-26 10:40:58 -0400

    enhanced effects test

--- src/test/scala/zio/ZioTest.scala
+++ src/test/scala/zio/ZioTest.scala
@@ -3,11 +3,17 @@ package zio
 import org.scalatest.{FunSuite, Matchers}
 import scalaz.zio.{DefaultRuntime, ZIO}
 
+import scala.util.Try
+
 class ZioTest extends FunSuite with Matchers {
   test("effects") {
     val runtime = new DefaultRuntime {}
 
     runtime.unsafeRun( ZIO.succeed(33) ) shouldBe 33
     runtime.unsafeRun( ZIO.succeedLazy( List(1, 2, 3).sum ) ) shouldBe 6
+    runtime.unsafeRun( ZIO.fromOption(Some(3)).map(_ * 2) ) shouldBe 6
+    runtime.unsafeRun( ZIO.fromEither(Right(3)).map(_ * 2) ) shouldBe 6
+    runtime.unsafeRun( ZIO.fromTry(Try(18 / 3)) ) shouldBe 6
+    runtime.unsafeRun( ZIO.fromFunction((i: Int) => i * i).provide(3) ) shouldBe 9
   }
 }
