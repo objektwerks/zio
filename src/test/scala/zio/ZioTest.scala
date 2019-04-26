1f15d45e4104fb08675cda153b75b3fcbd30e222
objektwerks <objektwerks@runbox.com>
2019-04-26 11:56:43 -0400

    added fail effect to effects test

--- src/test/scala/zio/ZioTest.scala
+++ src/test/scala/zio/ZioTest.scala
@@ -11,6 +11,7 @@ class ZioTest extends FunSuite with Matchers {
   test("effects") {
     val runtime = new DefaultRuntime {}
 
+    runtime.unsafeRun( ZIO.fail("fail").mapError(error => new Exception(error)).either ).left.toOption.nonEmpty shouldBe true
     runtime.unsafeRun( ZIO.succeed(33) ) shouldBe 33
     runtime.unsafeRun( ZIO.succeedLazy( List(1, 2, 3).sum ) ) shouldBe 6
     runtime.unsafeRun( ZIO.fromOption(Some(3)).map(_ * 2) ) shouldBe 6
