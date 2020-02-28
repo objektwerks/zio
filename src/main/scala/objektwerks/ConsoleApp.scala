0a474937218b4823f7b1143dd487a74565958c3c
objektwerks <objektwerks@runbox.com>
2020-02-28 08:37:45 -0500

    reorganized console app code

--- src/main/scala/objektwerks/ConsoleApp.scala
+++ src/main/scala/objektwerks/ConsoleApp.scala
@@ -4,14 +4,14 @@ import zio.{App, ZIO}
 import zio.console._
 
 object ConsoleApp extends App {
-  val error = 1
-  val success = 0
-
-  def run(args: List[String]): ZIO[Console, Nothing, Int] = program.fold(_ => error, _ => success)
-
   val program = for {
     _    <- putStrLn("Hello! What is your name?")
     name <- getStrLn
     _    <- putStrLn(s"Hello, $name, welcome to ZIO!")
   } yield ()
+
+  val error = 1
+  val success = 0
+
+  def run(args: List[String]): ZIO[Console, Nothing, Int] = program.fold(_ => error, _ => success)
 }
