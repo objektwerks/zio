e8617aaac9e81b597500ef9fcac228e46b525a2f
objektwerks <objektwerks@runbox.com>
2020-03-06 14:37:36 -0500

    correct error

--- src/test/scala/objektwerks/ManagedResourceTest.scala
+++ src/test/scala/objektwerks/ManagedResourceTest.scala
@@ -4,13 +4,13 @@ import zio.{Managed, Task, ZIO}
 
 import scala.io.{BufferedSource, Codec, Source}
 
-class ManagedResourceTest extends ZioTest{
+class ManagedResourceTest extends ZioTest {
   test("managed resource") {
     def resource(file: String): Task[BufferedSource] = ZIO.effect(Source.fromFile(file, Codec.UTF8.name))
     def close(source: BufferedSource): Task[Unit] = ZIO.effect(source.close)
     def tostring(source: BufferedSource): Task[String] = ZIO.effect(source.mkString)
 
-    val managed = Managed.make(resource("build.sbt"))(close(_).catchAll(_ => Task.unit))
+    val managed = Managed.make(resource("build.sbt"))(close(_).ignore)
     val task: Task[String] = managed.use { source => tostring(source) }
     runtime.unsafeRun( task ).nonEmpty shouldBe true
   }
