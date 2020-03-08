f0495e1e913407270574144f02da2f879d2ba9e4
objektwerks <objektwerks@runbox.com>
2020-03-08 10:24:04 -0400

    shortened zio boolean tests

--- src/test/scala/objektwerks/ZioTest.scala
+++ src/test/scala/objektwerks/ZioTest.scala
@@ -13,7 +13,7 @@ object ZioTest extends DefaultRunnableSpec {
 
   def spec: Spec[Environment, TestFailure[Nothing], TestSuccess] = suite("zio.spec.test")(
     test("effects") {
-      assert(runtime.unsafeRun( ZIO.fail("fail").mapError(error => new Exception(error)).either ).isLeft)(equalTo(true))
+      assert(runtime.unsafeRun( ZIO.fail("fail").mapError(error => new Exception(error)).either ).isLeft)(isTrue)
       assert(runtime.unsafeRun( ZIO.succeed(33) ))(equalTo(33))
       assert(runtime.unsafeRun( ZIO.effectTotal( List(1, 2, 3).sum )))(equalTo(6))
 
@@ -26,16 +26,16 @@ object ZioTest extends DefaultRunnableSpec {
 
     test("errors") {
       val fallback: Task[String] = file("build.sat") orElse file("build.sbt")
-      assert(runtime.unsafeRun( fallback ).nonEmpty)(equalTo(true))
+      assert(runtime.unsafeRun( fallback ).nonEmpty)(isTrue)
 
       val fold: Task[String] = file("build.sat").foldM(_ => file("build.sbt"), source => ZIO.succeed(source))
-      assert(runtime.unsafeRun( fold ).nonEmpty)(equalTo(true))
+      assert(runtime.unsafeRun( fold ).nonEmpty)(isTrue)
 
       val catchall: Task[String] = file("build.sat").catchAll(_ => file("build.sbt"))
-      assert(runtime.unsafeRun( catchall ).mkString.nonEmpty)(equalTo(true))
+      assert(runtime.unsafeRun( catchall ).mkString.nonEmpty)(isTrue)
 
       val retryOrElse: Task[String] = file("build.sat").retryOrElse( Schedule.once, (_: Throwable, _: Unit) => file("build.sbt"))
-      assert(runtime.unsafeRun( retryOrElse ).mkString.nonEmpty)(equalTo(true))
+      assert(runtime.unsafeRun( retryOrElse ).mkString.nonEmpty)(isTrue)
     },
 
     test("fiber ref") {
@@ -52,7 +52,7 @@ object ZioTest extends DefaultRunnableSpec {
         fiber <- file("build.sbt").fork
         source <- fiber.join
       } yield source.mkString
-      assert(runtime.unsafeRun( fileContent ).nonEmpty)(equalTo(true))
+      assert(runtime.unsafeRun( fileContent ).nonEmpty)(isTrue)
 
       val helloWorld = for {
         helloFiber <- ZIO.succeed("Hello, ").fork
@@ -73,7 +73,7 @@ object ZioTest extends DefaultRunnableSpec {
 
       val managed = Managed.make(resource("build.sbt"))(close(_).ignore)
       val task: Task[String] = managed.use { source => tostring(source) }
-      assert(runtime.unsafeRun( task ).nonEmpty)(equalTo(true))
+      assert(runtime.unsafeRun( task ).nonEmpty)(isTrue)
     },
 
     test("queue") {
