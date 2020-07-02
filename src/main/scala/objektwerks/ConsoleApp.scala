77ccc7222b810f65cbd2b160c215263748fa18da
objektwerks <objektwerks@runbox.com>
2020-07-02 10:18:06 -0400

    refactored ConsoleApp imports

--- src/main/scala/objektwerks/ConsoleApp.scala
+++ src/main/scala/objektwerks/ConsoleApp.scala
@@ -3,11 +3,7 @@ package objektwerks
 import java.io.IOException
 
 import zio.console._
-import zio.App
-import zio.ZIO
-import zio.URIO
-import zio.ExitCode
-import zio.ZEnv
+import zio.{App, ExitCode, URIO, ZEnv, ZIO}
 
 object ConsoleApp extends App {
   val program: ZIO[Console, IOException, Unit] = for {
