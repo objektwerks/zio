3f5c8ce48a67c904b1e905df687e9953c2f02eef
objektwerks <objektwerks@runbox.com>
2021-03-07 07:48:36 -0500

    added ConsoleLayerStore

---
+++ src/main/scala/objektwerks/ConsoleLayerStore.scala
@@ -0,0 +1,21 @@
+package objektwerks
+
+import zio.{ZIO, Has, Task, ZLayer}
+
+object ConsoleLayerStore {
+  type ConsoleLayerStoreEnv = Has[ConsoleLayerStore.Service]
+
+  trait Service {
+    def store(message: Message): Task[Unit]
+  }
+
+  val live: ZLayer[Any, Nothing, ConsoleLayerStoreEnv] = ZLayer.succeed {
+    new Service {
+      override def store(message: Message): Task[Unit] = Task {
+        println(s"[ConsoleLayerStore] stored: ${message.text}")
+      }
+    }
+  }
+
+  def store(message: Message): ZIO[ConsoleLayerStoreEnv, Throwable, Unit] = ZIO.accessM(_.get.store(message))
+}
