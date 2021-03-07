a3286f7c6ea36f3dd8db00604c221bfd85b9fe28
objektwerks <objektwerks@runbox.com>
2021-03-07 11:13:45 -0500

    renamed layer vals for clarity

--- src/main/scala/objektwerks/ConsoleHorizontalLayerApp.scala
+++ src/main/scala/objektwerks/ConsoleHorizontalLayerApp.scala
@@ -6,8 +6,7 @@ object ConsoleHorizontalLayerApp extends zio.App {
   import ConsolePrinter._
   import ConsoleStore._
 
-  val serviceStoreLayer: ZLayer[Any, Nothing, Printer with Store] =
-    ConsolePrinter.live ++ ConsoleStore.live
+  val printerStoreHorizontalLayer: ZLayer[Any, Nothing, Printer with Store] = ConsolePrinter.live ++ ConsoleStore.live
 
   val program: ZIO[Printer with Store, Throwable, Message] = for {
     m  <- print( Message("Horizontal layer app message!") )
@@ -16,7 +15,7 @@ object ConsoleHorizontalLayerApp extends zio.App {
 
   def run(args: List[String]): URIO[ZEnv, ExitCode] =
     program
-      .provideLayer(serviceStoreLayer)
+      .provideLayer(printerStoreHorizontalLayer)
       .catchAll(error => ZIO.succeed( error.printStackTrace() ).map(_ => ExitCode.failure) )
       .map { message =>
         println(s"[App] Printed and stored message: $message")
--- src/main/scala/objektwerks/ConsoleVerticalLayerApp.scala
+++ src/main/scala/objektwerks/ConsoleVerticalLayerApp.scala
@@ -7,15 +7,12 @@ object ConsoleVerticalLayerApp extends zio.App {
   import ConsoleStore._
   import ConsolePrinterStore._
 
-  val serviceStoreLayer: ZLayer[Any, Nothing, Printer with Store] =
-    ConsolePrinter.live ++ ConsoleStore.live
-
-  val compositeLayer: ZLayer[Any, Throwable, PrinterStore] =
-    serviceStoreLayer >>> ConsolePrinterStore.live
+  val printerStoreHorizontalLayer: ZLayer[Any, Nothing, Printer with Store] = ConsolePrinter.live ++ ConsoleStore.live
+  val printerStoreVerticalLayer: ZLayer[Any, Throwable, PrinterStore] = printerStoreHorizontalLayer >>> ConsolePrinterStore.live
 
   override def run(args: List[String]): ZIO[ZEnv, Nothing, ExitCode] =
     ConsolePrinterStore.printAndStore( Message("Vertical layer app message!") )
-      .provideLayer(compositeLayer)
+      .provideLayer(printerStoreVerticalLayer)
       .catchAll(error => ZIO.succeed( error.printStackTrace() ).map(_ => ExitCode.failure) )
       .map { message =>
         println(s"[App] Printed and stored message: $message")
