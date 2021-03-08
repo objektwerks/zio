0eed1211a5678477c276f12cc4d7e0b816ee958e
objektwerks <objektwerks@runbox.com>
2021-03-08 08:56:33 -0500

    edited console app val name

--- src/main/scala/objektwerks/ConsoleApp.scala
+++ src/main/scala/objektwerks/ConsoleApp.scala
@@ -6,11 +6,11 @@ import zio.console._
 import zio.{App, ExitCode, URIO, ZEnv, ZIO}
 
 object ConsoleApp extends App {
-  val app: ZIO[Console, IOException, Unit] = for {
+  val effect: ZIO[Console, IOException, Unit] = for {
     _    <- putStrLn("Hello! What is your name?")
     name <- getStrLn
     _    <- putStrLn(s"Hello, $name, welcome to ZIO!")
   } yield ()
 
-  def run(args: List[String]): URIO[ZEnv, ExitCode] = app.fold(_ => ExitCode.failure, _ => ExitCode.success)
+  def run(args: List[String]): URIO[ZEnv, ExitCode] = effect.fold(_ => ExitCode.failure, _ => ExitCode.success)
 }
