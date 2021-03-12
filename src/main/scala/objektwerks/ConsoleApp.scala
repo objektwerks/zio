e24ca233c9a178077bdb251d3171ec8550dae6ab
objektwerks <objektwerks@runbox.com>
2021-03-12 11:40:54 -0500

    refactored ConsoleConfig and App

--- src/main/scala/objektwerks/ConsoleApp.scala
+++ src/main/scala/objektwerks/ConsoleApp.scala
@@ -6,10 +6,12 @@ import zio.console._
 import zio.{App, ExitCode, URIO, ZEnv, ZIO}
 
 object ConsoleApp extends App {
+  val conf = ConsoleConfig.load("./src/main/resources/console.conf").getOrElse(ConsoleConfig.empty)
+
   val effect: ZIO[ZEnv, IOException, Unit] = for {
-    _    <- putStrLn("What is your name?")
+    _    <- putStrLn(conf.question)
     name <- getStrLn
-    _    <- putStrLn(s"$name, welcome to ZIO!")
+    _    <- putStrLn(s"$name, ${conf.response}")
   } yield ()
 
   def run(args: List[String]): URIO[ZEnv, ExitCode] = effect.fold(_ => ExitCode.failure, _ => ExitCode.success)
--- src/main/scala/objektwerks/ConsoleConfig.scala
+++ src/main/scala/objektwerks/ConsoleConfig.scala
@@ -2,11 +2,19 @@ package objektwerks
 
 import java.io.File
 
+import zio.config._
 import zio.config.magnolia.DeriveConfigDescriptor.descriptor
-import zio.config.typesafe.TypesafeConfig._
+import zio.config.typesafe.TypesafeConfigSource
 
 case class ConsoleConfig(question: String, response: String)
 
 object ConsoleConfig {
-  def read(path: String) = fromHoconFile(new File(path), descriptor[ConsoleConfig])
+  val empty = ConsoleConfig("", "")
+  val desc = descriptor[ConsoleConfig]
+
+  def load(path: String): Either[ReadError[String], ConsoleConfig] =
+    TypesafeConfigSource.fromHoconFile( new File(path) ) match {
+      case Right(source) => read(descriptor[ConsoleConfig] from source)
+      case Left(error) => Left(error)
+    }
 }
