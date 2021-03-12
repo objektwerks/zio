191016075dde09567cd616d64a445dabb8b14a86
objektwerks <objektwerks@runbox.com>
2021-03-12 09:30:00 -0500

    added read method to ConsoleConfig

--- src/main/scala/objektwerks/ConsoleConfig.scala
+++ src/main/scala/objektwerks/ConsoleConfig.scala
@@ -3,11 +3,10 @@ package objektwerks
 import java.io.File
 
 import zio.config.magnolia.DeriveConfigDescriptor.descriptor
-import zio.config.typesafe.TypesafeConfig
+import zio.config.typesafe.TypesafeConfig._
 
 case class ConsoleConfig(question: String, response: String)
 
 object ConsoleConfig {
-  val derived = descriptor[ConsoleConfig]
-  val live = TypesafeConfig.fromHoconFile(new File("/console.conf"), derived)
+  def read(path: String) = fromHoconFile(new File(path), descriptor[ConsoleConfig])
 }
