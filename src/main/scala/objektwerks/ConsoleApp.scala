b943e06e08fe061a8fee504d2ef89aafce2d22e2
objektwerks <objektwerks@runbox.com>
2021-03-12 14:52:30 -0500

    added comment to ConsoleApp

--- src/main/scala/objektwerks/ConsoleApp.scala
+++ src/main/scala/objektwerks/ConsoleApp.scala
@@ -6,6 +6,7 @@ import zio.console._
 import zio.{App, ExitCode, URIO, ZEnv, ZIO}
 
 object ConsoleApp extends App {
+  // How to mixin a zio-config layer in lieu of the below code?
   val conf = ConsoleConfig.toConfig("console.conf")
 
   val effect: ZIO[ZEnv, IOException, Unit] = for {
