e67bcbe545dd8ee5a44292bb45ba75df46c0bdda
objektwerks <objektwerks@runbox.com>
2021-03-14 08:29:44 -0400

    added zio logging; edited console app type signatures

--- build.sbt
+++ build.sbt
@@ -12,6 +12,7 @@ libraryDependencies ++= {
     "dev.zio" %% "zio-config" % zioConfigVersion,
     "dev.zio" %% "zio-config-magnolia" % zioConfigVersion,
     "dev.zio" %% "zio-config-typesafe" % zioConfigVersion,
+    "dev.zio" %% "zio-logging" % "0.5.7",
     "dev.zio" %% "zio-test" % zioVersion % Test,
     "dev.zio" %% "zio-test-sbt" % zioVersion % Test
   )
--- src/main/scala/objektwerks/ConsoleApp.scala
+++ src/main/scala/objektwerks/ConsoleApp.scala
@@ -3,18 +3,18 @@ package objektwerks
 import java.io.IOException
 
 import zio.console._
-import zio.{App, ExitCode, URIO, ZEnv, ZIO}
+import zio.{App, ExitCode, URIO, ZIO}
 
 object ConsoleApp extends App {
   // How to mixin a zio-config layer in lieu of the below code?
   // See ConsoleConfig and ConsolePrinterStoreLayers
   val conf = ConsoleConfig.toConfig("console.conf")
 
-  val effect: ZIO[ZEnv, IOException, Unit] = for {
+  val effect: ZIO[Console, IOException, Unit] = for {
     _    <- putStrLn(conf.question)
     name <- getStrLn
     _    <- putStrLn(s"$name, ${conf.response}")
   } yield ()
 
-  def run(args: List[String]): URIO[ZEnv, ExitCode] = effect.fold(_ => ExitCode.failure, _ => ExitCode.success)
+  def run(args: List[String]): URIO[Console, ExitCode] = effect.fold(_ => ExitCode.failure, _ => ExitCode.success)
 }
