b4b327e473078676fe07fd062f0467947b92a18e
objektwerks <objektwerks@runbox.com>
2021-03-16 13:56:41 -0400

    refactored effect test

--- src/test/scala/objektwerks/EffectTest.scala
+++ src/test/scala/objektwerks/EffectTest.scala
@@ -9,16 +9,36 @@ import scala.util.Try
 
 object EffectTest extends ZioTest {
   def spec: Spec[Environment, TestFailure[Nothing], TestSuccess] = suite("effect.test")(
-    test("effect") {
-      assert(runtime.unsafeRun( ZIO.fail("fail").mapError(error => new Exception(error)).either ).isLeft)(isTrue)
-      assert(runtime.unsafeRun( ZIO.succeed(33) ))(equalTo(33))
+    test("effect total") {
       assert(runtime.unsafeRun( ZIO.effectTotal( List(1, 2, 3).sum )))(equalTo(6))
+    },
 
-      assert(runtime.unsafeRun( ZIO.fromOption(Some(3)).map(_ * 2) ))(equalTo(6))
-      assert(runtime.unsafeRun( ZIO.fromEither(Right(3)).map(_ * 2) ))(equalTo(6))
-      assert(runtime.unsafeRun( ZIO.fromTry(Try(18 / 3)) ))(equalTo(6))
-      assert(runtime.unsafeRun( ZIO.fromFunction((i: Int) => i * i).provide(3) ))(equalTo(9))
+    test("from future") {
       assert(runtime.unsafeRun( ZIO.fromFuture { implicit ec => Future(3).map(_ * 3) } ))(equalTo(9))
+    },
+
+    test("fail") {
+      assert(runtime.unsafeRun( ZIO.fail("fail").mapError(error => new Exception(error)).either ).isLeft)(isTrue)
+    },
+
+    test("succeed") {
+      assert(runtime.unsafeRun( ZIO.succeed(9) ))(equalTo(9))
+    },
+
+    test("from option") {
+      assert(runtime.unsafeRun( ZIO.fromOption(Some(3)).map(_ * 2) ))(equalTo(6))
+    },
+
+    test("from either") {
+      assert(runtime.unsafeRun( ZIO.fromEither(Right(3)).map(_ * 2) ))(equalTo(6))
+    },
+
+    test("from try") {
+      assert(runtime.unsafeRun( ZIO.fromTry(Try(18 / 3)) ))(equalTo(6))
+    },
+
+    test("from function") {
+      assert(runtime.unsafeRun( ZIO.fromFunction((i: Int) => i * i).provide(3) ))(equalTo(9))
     }
   )
 }
