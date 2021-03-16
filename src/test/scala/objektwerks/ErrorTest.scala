cd8ece910dea123864f7080a50a4d8df7187230d
objektwerks <objektwerks@runbox.com>
2021-03-16 15:40:05 -0400

    refactored dependencies

--- src/test/scala/objektwerks/ErrorTest.scala
+++ src/test/scala/objektwerks/ErrorTest.scala
@@ -5,14 +5,20 @@ import zio.test._
 import zio.{Task, ZIO}
 
 object ErrorTest extends ZioTest {
+  import Files._
+
   def spec: Spec[Environment, TestFailure[Nothing], TestSuccess] = suite("error.test")(
-    test("error") {
+    test("fallback") {
       val fallback: Task[String] = file("build.sat") orElse file("build.sbt")
       assert(runtime.unsafeRun( fallback ).nonEmpty)(isTrue)
+    },
 
+    test("fold") {
       val fold: Task[String] = file("build.sat").foldM(_ => file("build.sbt"), source => ZIO.succeed(source))
       assert(runtime.unsafeRun( fold ).nonEmpty)(isTrue)
+    },
 
+    test("catch all") {
       val catchall: Task[String] = file("build.sat").catchAll(_ => file("build.sbt"))
       assert(runtime.unsafeRun( catchall ).mkString.nonEmpty)(isTrue)
     }
