c178d3e24e6ac8993315a518aa8737023be84603
objektwerks <objektwerks@runbox.com>
2021-03-16 13:57:51 -0400

    refactored stream test; yet intellij still shows 3 errors

--- src/test/scala/objektwerks/StreamTest.scala
+++ src/test/scala/objektwerks/StreamTest.scala
@@ -6,8 +6,20 @@ import zio.test._
 
 object StreamTest extends ZioTest {
   def spec: Spec[Environment, TestFailure[Nothing], TestSuccess] = suite("stream.test")(
-    test("stream") {
+    test("fold left") {
       assert( runtime.unsafeRun( Stream(1, 2, 3).run(Sink.foldLeft[Int, Int](0)(_ + _)) ))(equalTo(6))
+    },
+
+    test("fold") {
+      assert( runtime.unsafeRun( Stream(1, 2, 3).fold(0)(_ + _) ) )(equalTo(6))
+    },
+
+    test("map > fold") {
+      assert( runtime.unsafeRun( Stream(1, 2, 3).map(_ * 2).fold(0)(_ + _)) )(equalTo(12))
+    },
+
+    test("merge > fold") {
+      assert( runtime.unsafeRun( Stream(1, 2, 3).merge(Stream(4, 5, 6)).fold(0)(_ + _)) )(equalTo(21))
     }
   )
 }
