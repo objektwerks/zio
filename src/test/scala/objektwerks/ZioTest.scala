ba04b508d7bd717b2ee7e760192a2990f0187b13
objektwerks <objektwerks@runbox.com>
2021-03-16 15:39:32 -0400

    extracted file functions

--- src/test/scala/objektwerks/ZioTest.scala
+++ src/test/scala/objektwerks/ZioTest.scala
@@ -1,23 +1,8 @@
 package objektwerks
 
 import zio.test._
-import zio.{Runtime, Task, ZIO}
-
-import scala.io.{BufferedSource, Codec, Source}
+import zio.{Runtime, ZEnv}
 
 trait ZioTest extends DefaultRunnableSpec {
-  protected val runtime: Runtime[zio.ZEnv] = Runtime.default
-
-  def file(file: String): Task[String] = {
-    def close(source: BufferedSource): Task[Unit] = ZIO.effect(source.close)
-
-    def read(source: BufferedSource): Task[String] = ZIO.effect(source.mkString)
-
-    def open(file: String): Task[String] =
-      ZIO
-        .effect(Source.fromFile(file, Codec.UTF8.name))
-        .bracket(close(_).ignore)(source => read(source))
-
-    open(file)
-  }
+  protected val runtime: Runtime[ZEnv] = Runtime.default
 }
