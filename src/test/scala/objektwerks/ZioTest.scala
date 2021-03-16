706cb71d00991a4cc1ea63779343652117ad9e76
objektwerks <objektwerks@runbox.com>
2021-03-16 14:08:58 -0400

    reformatted zio test for clarity

--- src/test/scala/objektwerks/ZioTest.scala
+++ src/test/scala/objektwerks/ZioTest.scala
@@ -10,10 +10,14 @@ trait ZioTest extends DefaultRunnableSpec {
 
   def file(file: String): Task[String] = {
     def close(source: BufferedSource): Task[Unit] = ZIO.effect(source.close)
+
     def read(source: BufferedSource): Task[String] = ZIO.effect(source.mkString)
-    def open(file: String): Task[String] = ZIO
-      .effect(Source.fromFile(file, Codec.UTF8.name))
-      .bracket(close(_).ignore)(source => read(source))
+
+    def open(file: String): Task[String] =
+      ZIO
+        .effect(Source.fromFile(file, Codec.UTF8.name))
+        .bracket(close(_).ignore)(source => read(source))
+
     open(file)
   }
 }
