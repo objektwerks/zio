5aa32713dd5a3c873f02c18f7b602f17e8c519e5
objektwerks <objektwerks@runbox.com>
2021-03-16 15:39:57 -0400

    refactored dependencies

--- src/test/scala/objektwerks/FiberTest.scala
+++ src/test/scala/objektwerks/FiberTest.scala
@@ -5,15 +5,11 @@ import zio.test.Assertion.{equalTo, isTrue}
 import zio.test._
 
 object FiberTest extends ZioTest {
-  def spec: Spec[Environment, TestFailure[Nothing], TestSuccess] = suite("fiber.test")(
-    test("fiber") {
-      val fileContent = for {
-        fiber <- file("build.sbt").fork
-        source <- fiber.join
-      } yield source.mkString
-      assert(runtime.unsafeRun( fileContent ).nonEmpty)(isTrue)
+  import Files._
 
-      val helloWorld = for {
+  def spec: Spec[Environment, TestFailure[Nothing], TestSuccess] = suite("fiber.test")(
+    test("fiber > hello world") {
+      val helloWorld: ZIO[Any, Nothing, String] = for {
         helloFiber <- ZIO.succeed("Hello, ").fork
         worldFiber <- ZIO.succeed("world!").fork
         fiber = helloFiber zip worldFiber
@@ -23,6 +19,14 @@ object FiberTest extends ZioTest {
         hello + world
       }
       assert(runtime.unsafeRun( helloWorld ))(equalTo("Hello, world!"))
+    },
+
+    test("fiber > file") {
+      val fileContent: ZIO[Any, Throwable, String] = for {
+        fiber <- file("build.sbt").fork
+        source <- fiber.join
+      } yield source.mkString
+      assert(runtime.unsafeRun(fileContent).nonEmpty)(isTrue)
     }
   )
 }
