52e1c63d5985e7fda73a31fd7e87cbc2e1c33f8d
objektwerks <objektwerks@runbox.com>
2021-03-16 13:35:43 -0400

    removed code that is no longer usable with streams

--- src/test/scala/objektwerks/StreamTest.scala
+++ src/test/scala/objektwerks/StreamTest.scala
@@ -7,9 +7,6 @@ import zio.test._
 object StreamTest extends ZioTest {
   def spec: Spec[Environment, TestFailure[Nothing], TestSuccess] = suite("stream.test")(
     test("stream") {
-      assert( runtime.unsafeRun( Stream(1, 2, 3).fold(0)(_ + _) ) )(equalTo(6))
-      assert( runtime.unsafeRun( Stream(1, 2, 3).map(_ * 2).fold(0)(_ + _)) )(equalTo(12))
-      assert( runtime.unsafeRun( Stream(1, 2, 3).merge(Stream(4, 5, 6)).fold(0)(_ + _)) )(equalTo(21))
       assert( runtime.unsafeRun( Stream(1, 2, 3).run(Sink.foldLeft[Int, Int](0)(_ + _)) ))(equalTo(6))
     }
   )
