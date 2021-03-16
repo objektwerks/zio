7c8e158814d4595ce2ccd33244f3ed7b03a26a4e
objektwerks <objektwerks@runbox.com>
2021-03-16 13:25:56 -0400

    refactored protect val runtime

--- src/test/scala/objektwerks/ZioTest.scala
+++ src/test/scala/objektwerks/ZioTest.scala
@@ -6,7 +6,7 @@ import zio.{Runtime, Task, ZIO}
 import scala.io.{BufferedSource, Codec, Source}
 
 trait ZioTest extends DefaultRunnableSpec {
-  val runtime = Runtime.default
+  protected val runtime: Runtime[zio.ZEnv] = Runtime.default
 
   def file(file: String): Task[String] = {
     def close(source: BufferedSource): Task[Unit] = ZIO.effect(source.close)
