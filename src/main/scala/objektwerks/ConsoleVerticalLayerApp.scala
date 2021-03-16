cec7c04848cede5871ee21afab729cf066ff8b0d
objektwerks <objektwerks@runbox.com>
2021-03-16 13:02:29 -0400

    refactored exit code

--- src/main/scala/objektwerks/ConsoleVerticalLayerApp.scala
+++ src/main/scala/objektwerks/ConsoleVerticalLayerApp.scala
@@ -9,7 +9,7 @@ object ConsoleVerticalLayerApp extends App {
   override def run(args: List[String]): ZIO[ZEnv, Nothing, ExitCode] =
     printAndStore( Message("Vertical layer app message!") )
       .provideLayer(printerStoreVerticalLayer)
-      .catchAll(error => ZIO.succeed( error.printStackTrace() ).map(_ => ExitCode.failure) )
+      .catchAll(error => ZIO.succeed(error.printStackTrace()).exitCode )
       .map { message =>
         println(s"[App] Printed and stored message: $message")
         ExitCode.success
