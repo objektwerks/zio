8792d3bc417cad40675f138f9b665fec4ceb34ed
objektwerks <objektwerks@runbox.com>
2021-03-17 09:37:11 -0400

    reorganized Files

--- src/main/scala/objektwerks/Files.scala
+++ src/main/scala/objektwerks/Files.scala
@@ -5,14 +5,14 @@ import zio.{Task, ZIO}
 import scala.io.{BufferedSource, Codec, Source}
 
 object Files {
-  def close(source: BufferedSource): Task[Unit] = ZIO.effect(source.close)
-
-  def read(source: BufferedSource): Task[String] = ZIO.effect(source.mkString)
-
   def open(file: String): Task[String] =
     ZIO
       .effect(Source.fromFile(file, Codec.UTF8.name))
       .bracket(close(_).ignore)(source => read(source))
+
+  def read(source: BufferedSource): Task[String] = ZIO.effect(source.mkString)
+
+  def close(source: BufferedSource): Task[Unit] = ZIO.effect(source.close)
 
   def file(file: String): Task[String] = open(file)
 }
