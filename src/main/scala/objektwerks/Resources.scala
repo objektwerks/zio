2b3bcd520e832d70dba39c4290963bcc488a911b
objektwerks <objektwerks@runbox.com>
2021-03-17 09:37:34 -0400

    reorganized Resources

--- src/main/scala/objektwerks/Resources.scala
+++ src/main/scala/objektwerks/Resources.scala
@@ -7,7 +7,7 @@ import scala.io.{BufferedSource, Codec, Source}
 object Resources {
   def resource(file: String): Task[BufferedSource] = ZIO.effect(Source.fromFile(file, Codec.UTF8.name))
 
-  def close(source: BufferedSource): Task[Unit] = ZIO.effect(source.close)
-
   def stringify(source: BufferedSource): Task[String] = ZIO.effect(source.mkString)
+
+  def close(source: BufferedSource): Task[Unit] = ZIO.effect(source.close)
 }
