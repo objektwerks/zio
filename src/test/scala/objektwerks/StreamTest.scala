11fa2b0fd3cb820fd6231818809e1108934b2890
objektwerks <objektwerks@runbox.com>
2021-03-18 10:18:50 -0400

    added testM

--- src/test/scala/objektwerks/StreamTest.scala
+++ src/test/scala/objektwerks/StreamTest.scala
@@ -6,20 +6,20 @@ import zio.test._
 
 object StreamTest extends ZioTest {
   def spec: Spec[Environment, TestFailure[Nothing], TestSuccess] = suite("stream.test")(
-    test("fold left") {
-      assert( runtime.unsafeRun( Stream(1, 2, 3).run(Sink.foldLeft[Int, Int](0)(_ + _)) ))(equalTo(6))
+    testM("fold left") {
+      assertM( Stream(1, 2, 3).run(Sink.foldLeft[Int, Int](0)(_ + _)) )( equalTo(6) )
     },
 
-    test("fold") {
-      assert( runtime.unsafeRun( Stream(1, 2, 3).fold(0)(_ + _) ) )(equalTo(6))
+    testM("fold") {
+      assertM( Stream(1, 2, 3).fold(0)(_ + _) )( equalTo(6) )
     },
 
-    test("map > fold") {
-      assert( runtime.unsafeRun( Stream(1, 2, 3).map(_ * 2).fold(0)(_ + _)) )(equalTo(12))
+    testM("map > fold") {
+      assertM( Stream(1, 2, 3).map(_ * 2).fold(0)(_ + _) )( equalTo(12) )
     },
 
-    test("merge > fold") {
-      assert( runtime.unsafeRun( Stream(1, 2, 3).merge(Stream(4, 5, 6)).fold(0)(_ + _)) )(equalTo(21))
+    testM("merge > fold") {
+      assertM( Stream(1, 2, 3).merge(Stream(4, 5, 6)).fold(0)(_ + _) )( equalTo(21) )
     }
   )
 }
