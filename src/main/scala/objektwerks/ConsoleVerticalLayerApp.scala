e3cce5c2c93187209e322c4a509bf501a747a371
objektwerks <objektwerks@runbox.com>
2021-03-18 08:00:58 -0400

    clarified source of effect

--- src/main/scala/objektwerks/ConsoleVerticalLayerApp.scala
+++ src/main/scala/objektwerks/ConsoleVerticalLayerApp.scala
@@ -4,10 +4,9 @@ import zio.{App, ExitCode, ZEnv, ZIO}
 
 object ConsoleVerticalLayerApp extends App {
   import ConsoleLayers._
-  import ConsolePrinterStore._
 
   override def run(args: List[String]): ZIO[ZEnv, Nothing, ExitCode] =
-    printAndStore( Message("Vertical layer app message!") )
+    ConsolePrinterStore.printAndStore( Message("Vertical layer app message!") )
       .provideLayer(printerStoreVerticalLayer)
       .catchAll(error => ZIO.succeed(error.printStackTrace()).exitCode )
       .map { message =>
