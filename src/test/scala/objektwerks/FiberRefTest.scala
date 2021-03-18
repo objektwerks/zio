1af50ae1b7953aa8e57a4d391c9ac7e327a1e444
objektwerks <objektwerks@runbox.com>
2021-03-18 09:59:47 -0400

    refactored to use testM/assert

--- src/test/scala/objektwerks/FiberRefTest.scala
+++ src/test/scala/objektwerks/FiberRefTest.scala
@@ -1,18 +1,17 @@
 package objektwerks
 
-import zio.{FiberRef, ZIO}
+import zio.FiberRef
 import zio.test.Assertion.equalTo
 import zio.test._
 
 object FiberRefTest extends ZioTest {
   def spec: Spec[Environment, TestFailure[Nothing], TestSuccess] = suite("fiber.ref.test")(
-    test("fiber.ref") {
-      val fiberRefInt: ZIO[Any, Nothing, Int] = for {
+    testM("fiber.ref") {
+      for {
         fiberRef <- FiberRef.make[Int](0)
         _        <- fiberRef.set(3)
         value    <- fiberRef.get
-      } yield value
-      assert(runtime.unsafeRun( fiberRefInt ))(equalTo(3))
+      } yield assert(value)( equalTo(3) )
     }
   )
 }
