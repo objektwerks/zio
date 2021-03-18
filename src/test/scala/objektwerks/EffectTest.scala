dfd2e5a244bd7d40e535f2378c0c10ee27726208
objektwerks <objektwerks@runbox.com>
2021-03-18 09:54:22 -0400

    added testM/assertM where possible

--- src/test/scala/objektwerks/EffectTest.scala
+++ src/test/scala/objektwerks/EffectTest.scala
@@ -1,7 +1,7 @@
 package objektwerks
 
 import zio.ZIO
-import zio.test.Assertion.{equalTo, isTrue}
+import zio.test.Assertion.{equalTo, isLeft}
 import zio.test._
 
 import scala.concurrent.Future
@@ -9,36 +9,36 @@ import scala.util.Try
 
 object EffectTest extends ZioTest {
   def spec: Spec[Environment, TestFailure[Nothing], TestSuccess] = suite("effect.test")(
-    test("fail") {
-      assert(runtime.unsafeRun( ZIO.fail("fail").mapError(error => new Exception(error)).either ).isLeft)(isTrue)
+    testM("fail") {
+      assertM( ZIO.fail("fail").mapError(error => new Exception(error)).either )( isLeft )
     },
 
-    test("succeed") {
-      assert(runtime.unsafeRun( ZIO.succeed(9) ))(equalTo(9))
+    testM("succeed") {
+      assertM( ZIO.succeed(9) )( equalTo(9) )
     },
 
-    test("from function") {
-      assert(runtime.unsafeRun( ZIO.fromFunction((i: Int) => i * i).provide(3) ))(equalTo(9))
+    testM("from function") {
+      assertM( ZIO.fromFunction((i: Int) => i * i).provide(3) )( equalTo(9) )
     },
 
-    test("from option") {
-      assert(runtime.unsafeRun( ZIO.fromOption(Some(3)).map(_ * 2) ))(equalTo(6))
+    testM("from either") {
+      assertM( ZIO.fromEither(Right(3)).map(_ * 2) )( equalTo(6) )
     },
 
-    test("from either") {
-      assert(runtime.unsafeRun( ZIO.fromEither(Right(3)).map(_ * 2) ))(equalTo(6))
+    testM("effect total") {
+      assertM( ZIO.effectTotal( List(1, 2, 3).sum ))( equalTo(6) )
     },
 
-    test("from try") {
-      assert(runtime.unsafeRun( ZIO.fromTry(Try(18 / 3)) ))(equalTo(6))
+    test("from option") { // Can't use with testM/assertM
+      assert( runtime.unsafeRun( ZIO.fromOption(Some(3)).map(_ * 2) ))( equalTo(6) )
     },
 
-    test("effect total") {
-      assert(runtime.unsafeRun( ZIO.effectTotal( List(1, 2, 3).sum )))(equalTo(6))
+    test("from try") { // Can't use with testM/assertM
+      assert( runtime.unsafeRun( ZIO.fromTry(Try(18 / 3)) ))( equalTo(6) )
     },
 
-    test("from future") {
-      assert(runtime.unsafeRun( ZIO.fromFuture { implicit ec => Future(3).map(_ * 3) } ))(equalTo(9))
+    test("from future") { // Can't use with testM/assertM
+      assert( runtime.unsafeRun( ZIO.fromFuture { implicit ec => Future(3).map(_ * 3) } ))( equalTo(9) )
     }
   )
 }
