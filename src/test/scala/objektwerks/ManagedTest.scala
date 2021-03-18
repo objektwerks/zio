690a35b60b6f8a0d84e8af93ea84a0e360e89bf9
objektwerks <objektwerks@runbox.com>
2021-03-18 10:08:13 -0400

    reformatted code

--- src/test/scala/objektwerks/ManagedTest.scala
+++ src/test/scala/objektwerks/ManagedTest.scala
@@ -13,7 +13,7 @@ object ManagedTest extends ZioTest {
     test("managed") {
       val managed: ZManaged[Any, Throwable, BufferedSource] = Managed.make(resource("build.sbt"))(close(_).ignore)
       val task: Task[String] = managed.use { source => stringify(source) }
-      assert(runtime.unsafeRun( task ).nonEmpty)(isTrue)
+      assert( runtime.unsafeRun( task ).nonEmpty )( isTrue )
     }
   )
 }
