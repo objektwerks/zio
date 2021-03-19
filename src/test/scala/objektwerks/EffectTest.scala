edeece647862fde27bae4e17679785c18fee6739
objektwerks <objektwerks@runbox.com>
2021-03-19 05:31:52 -0400

    reformatted test

--- src/test/scala/objektwerks/EffectTest.scala
+++ src/test/scala/objektwerks/EffectTest.scala
@@ -10,7 +10,7 @@ import scala.util.Try
 object EffectTest extends ZioTest {
   def spec: Spec[Environment, TestFailure[Nothing], TestSuccess] = suite("effect.test")(
     testM("fail") {
-      assertM( ZIO.fail("fail").mapError(error => new Exception(error)).either )( isLeft )
+      assertM( ZIO.fail("fail").mapError( error => new Exception(error) ).either )( isLeft )
     },
 
     testM("succeed") {
@@ -18,27 +18,27 @@ object EffectTest extends ZioTest {
     },
 
     testM("from function") {
-      assertM( ZIO.fromFunction((i: Int) => i * i).provide(3) )( equalTo(9) )
+      assertM( ZIO.fromFunction( (i: Int) => i * i ).provide(3) )( equalTo(9) )
     },
 
     testM("from either") {
-      assertM( ZIO.fromEither(Right(3)).map(_ * 2) )( equalTo(6) )
+      assertM( ZIO.fromEither( Right(3) ).map( _ * 2 ) )( equalTo(6) )
     },
 
     testM("effect total") {
-      assertM( ZIO.effectTotal( List(1, 2, 3).sum ))( equalTo(6) )
+      assertM( ZIO.effectTotal( List(1, 2, 3).sum ) )( equalTo(6) )
     },
 
     test("from option") { // Can't use with testM/assertM
-      assert( runtime.unsafeRun( ZIO.fromOption(Some(3)).map(_ * 2) ))( equalTo(6) )
+      assert( runtime.unsafeRun( ZIO.fromOption( Some(3) ).map( _ * 2 ) ) )( equalTo(6) )
     },
 
     test("from try") { // Can't use with testM/assertM
-      assert( runtime.unsafeRun( ZIO.fromTry(Try(18 / 3)) ))( equalTo(6) )
+      assert( runtime.unsafeRun( ZIO.fromTry( Try(18 / 3) ) ) )( equalTo(6) )
     },
 
     test("from future") { // Can't use with testM/assertM
-      assert( runtime.unsafeRun( ZIO.fromFuture { implicit ec => Future(3).map(_ * 3) } ))( equalTo(9) )
+      assert( runtime.unsafeRun( ZIO.fromFuture { implicit ec => Future(3).map( _ * 3 ) } ) )( equalTo(9) )
     }
   )
 }
