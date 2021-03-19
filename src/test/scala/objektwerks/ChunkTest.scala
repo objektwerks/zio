cc2819176654c139d1e903d63bc7fabfa9dd1e7a
objektwerks <objektwerks@runbox.com>
2021-03-19 05:29:34 -0400

    reformatted test

--- src/test/scala/objektwerks/ChunkTest.scala
+++ src/test/scala/objektwerks/ChunkTest.scala
@@ -9,31 +9,31 @@ object ChunkTest extends ZioTest {
 
   def spec: Spec[Environment, TestFailure[Nothing], TestSuccess] = suite("chunk.test")(
     test("++") {
-      assert(Chunk(1, 2, 3) ++ Chunk(4, 5, 6))(equalTo(Chunk(1, 2, 3, 4, 5, 6)))
+      assert( Chunk(1, 2, 3) ++ Chunk(4, 5, 6) )( equalTo( Chunk(1, 2, 3, 4, 5, 6) ) )
     },
 
     test("collect") {
-      assert(chunk.collect(i => i * 2))(equalTo(Chunk(2, 4, 6)))
+      assert( chunk.collect(i => i * 2) )( equalTo( Chunk(2, 4, 6) ) )
     },
 
     test("collect while") {
-      assert(chunk.collectWhile { case i if i < 3 => i } )(equalTo(Chunk(1, 2)))
+      assert(chunk.collectWhile { case i if i < 3 => i } )( equalTo( Chunk(1, 2) ) )
     },
 
     test("drop") {
-      assert(chunk.drop(2))(equalTo(Chunk(3)))
+      assert( chunk.drop(2) )( equalTo( Chunk(3) ) )
     },
 
     test("drop while") {
-      assert(chunk.dropWhile(_ % 2 != 0))(equalTo(Chunk(2, 3)))
+      assert( chunk.dropWhile(_ % 2 != 0) )( equalTo( Chunk(2, 3) ) )
     },
 
     test("take") {
-      assert(chunk.take(2))(equalTo(Chunk(1, 2)))
+      assert( chunk.take(2) )( equalTo( Chunk(1, 2) ) )
     },
 
     test("take while") {
-      assert(chunk.takeWhile(_ % 2 != 0))(equalTo(Chunk(1)))
+      assert( chunk.takeWhile(_ % 2 != 0) )( equalTo( Chunk(1) ) )
     }
   )
 }
