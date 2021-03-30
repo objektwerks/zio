b44e1a364185fe4178dd2baccb3f830654ddcca2
objektwerks <objektwerks@runbox.com>
2021-03-30 14:39:29 -0400

    added NowClient

---
+++ src/main/scala/objektwerks/http/NowClient.scala
@@ -0,0 +1,24 @@
+package objektwerks.http
+
+import zhttp.http.Response.HttpResponse
+import zhttp.http._
+import zhttp.service._
+import zio._
+
+object NowClient extends App {
+  val env: ZLayer[Any, Nothing, ChannelFactory with EventLoopGroup] = ChannelFactory.nio ++ EventLoopGroup.auto()
+  val url: URL = URL( Root / "now" )
+  val endpoint: Endpoint = ( Method.GET, url )
+
+  val effect = for {
+    response <- Client.request( Request( endpoint ) )
+    _        <- console.putStrLn {
+                  response.asInstanceOf[HttpResponse].content match {
+                    case HttpContent.Complete(data) => data
+                    case _     => "No response received!"
+                  }
+                }
+  } yield ()
+
+  override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = effect.provideCustomLayer(env).exitCode
+}
