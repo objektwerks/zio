dc2ae0eb455c204ad31636940ba90a52118cf7c4
objektwerks <objektwerks@runbox.com>
2021-08-24 06:06:31 -0400

    moved json entities to entity

--- src/main/scala/objektwerks/Entity.scala
+++ src/main/scala/objektwerks/Entity.scala
@@ -1,4 +1,14 @@
 package objektwerks
 
+import zio.json.{DeriveJsonDecoder, DeriveJsonEncoder, JsonDecoder, JsonEncoder}
+
 sealed trait Entity extends Product with Serializable
+
 case class Message(text: String) extends Entity
+
+final case class Person(name: String, age: Int) extends Entity
+
+object Person {
+  implicit val decoder: JsonDecoder[Person] = DeriveJsonDecoder.gen[Person]
+  implicit val encoder: JsonEncoder[Person] = DeriveJsonEncoder.gen[Person]
+}
--- src/test/scala/objektwerks/JsonTest.scala
+++ src/test/scala/objektwerks/JsonTest.scala
@@ -4,19 +4,12 @@ import zio.json._
 import zio.test.Assertion.{equalTo, isTrue}
 import zio.test.{Spec, TestFailure, TestSuccess, assert}
 
-sealed trait Entity extends Product with Serializable
-final case class Person(name: String, age: Int) extends Entity
-object Person {
-  implicit val decoder: JsonDecoder[Person] = DeriveJsonDecoder.gen[Person]
-  implicit val encoder: JsonEncoder[Person] = DeriveJsonEncoder.gen[Person]
-}
-
 object JsonTest extends ZioTest {
   import Person._
 
   def spec: Spec[Environment, TestFailure[Nothing], TestSuccess] = suite("json.test")(
     test("json") {
-      val person = Person("Fred Flintstone", 39)
+      val person = Person("Fred Flintstone", 25)
       val json = person.toJson
       val other = json.fromJson.getOrElse( Person("", -1) )
       assert( other )( equalTo( person ) ) &&
