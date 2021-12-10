6c5b34f94bdf63c365da2ad00ebbb71a0c79fcaf
objektwerks <objektwerks@runbox.com>
2021-12-10 06:22:04 -0500

    replaced Root with !! in zhttp server

--- src/main/scala/objektwerks/http/NowServer.scala
+++ src/main/scala/objektwerks/http/NowServer.scala
@@ -9,7 +9,7 @@ import zio._
 
 object NowServer extends App {
   val app: HttpApp[Any, Nothing] = Http.collect {
-    case Method.GET -> Root / "now" => Response.text( Instant.now.toString )
+    case Method.GET -> !! / "now" => Response.text( Instant.now.toString )
   }
 
   override def run(args: List[String]): URIO[ZEnv, ExitCode] = Server.start(7979, app).exitCode
